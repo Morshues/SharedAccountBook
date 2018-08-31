78be7426147dcd54ea29abe2f81e0363d50527f8
Morshues <morshues@gmail.com>
2018-08-31 19:52:12 +0800

    Let oauth login can use the same email with different providers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
   # Attributes related macros
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, :rememberable,
          :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   # association macros
@@ -24,7 +23,6 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name  
       user.image = auth.info.image 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -68,8 +68,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_132423) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -77,8 +75,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_132423) do
     t.string "uid"
     t.string "name", default: "", null: false
     t.string "image", default: "", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
