f25e7b6fc1e0edde15b2757a173f3af420f42f16
Morshues <morshues@gmail.com>
2018-09-04 19:32:58 +0800

    Fix jquery not found problem.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.append('expose-jquery', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: '$'
+  }, {
+    loader: 'expose-loader',
+    options: 'jQuery'
+  }]
+})
+
 module.exports = environment
